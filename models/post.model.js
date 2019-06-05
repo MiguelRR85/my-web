3d9c4e4a5acc884166453608b8b1b0d5c39343fe
Miguel <miguel.arr085@gmail.com>
2019-06-05 20:58:05 +0200

    Modificación de estilos en las páginas principales

--- models/post.model.js
+++ models/post.model.js
@@ -9,6 +9,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: 'The post content is required'
   },
+  resumeContent: {
+    type: String,
+    required: 'The post resume is required'
+  },
   image: String,
   imagePath: String,
   nameUser:String,
