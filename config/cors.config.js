00b4fa60de533839b9e4d8e4cf5a35a9abb2f903
Miguel <miguel.arr085@gmail.com>
2019-12-01 16:11:44 +0100

    cambio de texto página principal

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,6 +1,6 @@
 
 
-const originsAllowed = process.env.CORS_ORIGINS2 || process.env.CORS_ORIGINS || [
+const originsAllowed = process.env.CORS_ORIGINS || process.env.CORS_ORIGINS2 || [
   'http://localhost:3000',
   'http://localhost:4200',
   
